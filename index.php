19a3f7761723abc8eff44fe2c231c25b771a2471
Genmar Dadivo <31000214+genmar-dadivo@users.noreply.github.com>
2020-12-14 10:19:27 +0800

    Sample header form
    imitate column arrangement base on branch excel.

--- index.php
+++ index.php
@@ -2,8 +2,102 @@
 <html>
 <head>
 	<title> Macola </title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bs/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/animate/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main/main.css">
 </head>
 <body>
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Order type" autocomplete="off" required>
+			</div>
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Order number" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Status" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" onfocus="this.type='date'" placeholder="Order Date" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Apply To Number" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Customer Number" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" onfocus="this.type='date'" placeholder="Shipping Date" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Ship Code" autocomplete="off" required>
+			</div>
+			<div class="col">
+				<input type="text" class="form-control" placeholder="AR Terms Code" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Sales Person" autocomplete="off" required>
+			</div>
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Tax Code" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="MFG Location" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Total Sales" autocomplete="off" required>
+			</div>
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Total Tax" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" onfocus="this.type='date'" placeholder="Picked Date" autocomplete="off" required>
+			</div>
+			<div class="col">
+				<input type="text" class="form-control" onfocus="this.type='date'" placeholder="Billed Date" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Invoice No" autocomplete="off" required>
+			</div>
+			<div class="col">
+				<input type="text" class="form-control" placeholder="Invoice Date" autocomplete="off" required>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col">
+				<input type="text" class="form-control" onfocus="this.type='date'" placeholder="Posted Date" autocomplete="off" required>
+			</div>
+		</div>
+	</div>
 
+	<script type="text/javascript" src="assets/js/jquery/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bs/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/bs-notify/bootstrap-notify.min.js"></script>
+	<script type="text/javascript" src="assets/js/popper/popper.min.js"></script>
+	
+	<script type="text/javascript" src="assets/js/main/main.js"></script>
 </body>
 </html>
